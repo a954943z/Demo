e90de8cc30a00abfc2c6195412ec88a08baa0121
a954943z <102521256+a954943z@users.noreply.github.com>
2022-04-19 22:59:59 +0800

    新增註冊登入功能

--- DEMO/DEMO/Controllers/AccountController.cs
+++ DEMO/DEMO/Controllers/AccountController.cs
@@ -9,9 +9,51 @@ namespace DEMO.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
-        {           
+        private TrueDB _TrueDB;
+
+        public AccountController(TrueDB trueDB)
+        {
+            _TrueDB = trueDB;
+        }
+
+        public IActionResult LoginView()
+        {
             return View();
+        }
+        public IActionResult RegisterView()
+        {
+            return View();
+        }
+
+        public bool Login([FromBody] Models.ViewModels.LoginViewModel loginViewModel)
+        {
+            var userlogin = _TrueDB.users.FirstOrDefault(x => x.Name == loginViewModel.Name && x.Password==loginViewModel.Password);
+            if (userlogin!=null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Register([FromBody] Models.ViewModels.RegisterViewModel registerViewModel)
+        {
+            var userRegister = _TrueDB.users.FirstOrDefault(x => x.Name == registerViewModel.Name);
+            if(userRegister==null)
+            {
+                _TrueDB.users.Add(new Models.User()
+                {
+                    Name = registerViewModel.Name,
+                    Password = registerViewModel.Password
+                });
+                _TrueDB.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
---
+++ DEMO/DEMO/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DEMO.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Name { get; set; }
+        public int Password { get; set; }
+    }
+}
